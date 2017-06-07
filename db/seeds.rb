ef5fb2d9706d243e820cbc9bb2aa160f86cb66b2
jonathankim407 <jonathankim407@gmail.com>
2017-06-07 16:58:12 -0400

    05/03/01/created a new model for Player and migrated the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Player.create(
+  name:  'Steph Curry',
+  position: 'PG',
+  url:    'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201939.png'
+)
+
+Player.create(
+  name:  'Kevin Durant',
+  position: 'SF',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201142.png'
+)
+
+Player.create(
+  name:  'LeBron James',
+  position: 'SF',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/2544.png'
+)
