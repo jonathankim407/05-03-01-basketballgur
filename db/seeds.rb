705940c4121035e9c3bb433c34cfa331ed4faa58
jonathankim407 <jonathankim407@gmail.com>
2017-06-08 19:42:08 -0400

    Creating some test data, Rails console, Displaying data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create(
+  name:  'Russell Westbrook',
+  position: 'PG',
+  college: 'UCLA',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201566.png'
+)
+
+Player.create(
+  name:  'Stephen Curry',
+  position: 'PG',
+  college: 'Davidson',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201939.png'
+)
+
+Player.create(
+  name:  'Isaiah Thomas',
+  position: 'PG',
+  college: 'Washington',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/202738.png'
+)
+
+Player.create(
+  name:  'Jimmy Butler',
+  position: 'SF',
+  college: 'Marquette',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/202710.png'
+)
+
+Player.create(
+  name:  'Kawhi Leonard',
+  position: 'SF',
+  college: 'San Diego State',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/202695.png'
+)
+
+Player.create(
+  name:  'LeBron James',
+  position: 'SF',
+  college: 'St. Vincent-St. Mary HS (OH)',
+  url:    'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/2544.png'
+)
