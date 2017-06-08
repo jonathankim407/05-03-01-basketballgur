9b6987166a3a946ee5e6f42978159fc08787d2fd
jonathankim407 <jonathankim407@gmail.com>
2017-06-08 19:59:44 -0400

    Saving new players to the database

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,7 +12,18 @@ class PlayersController < ApplicationController
   end
 
   def create
-    render text: "Received POST request to '/players' with the data URL: #{params}"
+    @player = Player.new(player_params)
+    if @player.save
+      # if the player gets saved, generate a get request to "/players" (the index)
+      redirect_to "/players"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
+  end
+
+  def player_params
+    { name: params[:player][:name], position: params[:player][:position], college: params[:player][:college], url: params[:player][:url] }
   end
 
 end
